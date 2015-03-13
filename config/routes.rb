0e1f00c5f3d67ee1cef21c4ff8cfa11b56cd9cfe
davefogo <davefogo@gmail.com>
2015-03-13 17:50:16 -0500

    contructed bare bones nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'bienvenidos/index'
+
   resources :medios
 
   resources :proveedores
@@ -11,7 +13,7 @@ Rails.application.routes.draw do
 
   resources :colaboradores
 
-  root to: 'welcome#index'
+  root to: 'bienvenidos#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
