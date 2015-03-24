99834254ffeac1c83294c0b8ac13c817feb7108d
davefogo <davefogo@gmail.com>
2015-03-24 17:46:54 -0500

    fixing everything to work as before

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -69,6 +69,6 @@ class ClientesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cliente_params
-      params.require(:cliente).permit(:nombre, :nit, :dirección_de_facturación, :teléfono, :tipo_de_cliente, :contacto_comercial, :contacto_facturación, :cupo_de_crédito, :colaboradore_id)
+      params.require(:cliente).permit(:nombre, :nit, :direccion_de_facturacion, :telefono, :tipo_de_cliente, :contacto_comercial, :contacto_facturacion, :cupo_de_credito, :colaboradore)
     end
 end
