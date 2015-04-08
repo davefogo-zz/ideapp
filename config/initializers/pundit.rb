80ee8a605186278299d0fa2924aa35c135a9740d
davefogo <davefogo@gmail.com>
2015-04-08 18:18:44 -0500

    Finished adjusting to Rails Composer rails_devise, still error in no_reply@ at devise.rb

--- config/initializers/pundit.rb
+++ config/initializers/pundit.rb
@@ -1,4 +1,4 @@
-#config/initializers/pundit.rb
+# config/initializers/pundit.rb
 # Extends the ApplicationController to add Pundit for authorization.
 # Modify this file to change the behavior of a 'not authorized' error.
 # Be sure to restart your server when you modify this file.
