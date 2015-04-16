9dcc4fcdf47100abd08a484a0c52c7361bac871b
David Forero <davefogo@gmail.com>
2015-04-16 10:20:13 -0500

    began adding validations

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,4 +1,7 @@
 class Cliente < ActiveRecord::Base
   belongs_to :colaboradore
   has_many :presupuestos
+  validates :nombre, :nit, presence: true, uniqueness: true
+  validates :nit, length: {is: 11} 
+  
 end
--- app/models/colaboradore.rb
+++ app/models/colaboradore.rb
@@ -1,3 +1,4 @@
 class Colaboradore < ActiveRecord::Base
   has_many :clientes
+  validates :cedula, :nombre, :presence true
 end
