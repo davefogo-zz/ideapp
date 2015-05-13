6cec460a22cb03f4acda7667eca80d08d34da8e7
David Forero <davefogo@gmail.com>
2015-05-13 17:50:09 -0500

    added additional nav_links

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
+  enum role: [:admin, :user, :gerente, :gerente_comercial, :gerente_digital, :gerente_financiero, :contador, :recursoshumanos, :creativo, :ejecutivo, :analista_tesoreria, :analista_cartera, :recepcion_facturas, :analista_ordenacion, :analista_estudios]
   after_initialize :set_default_role, :if => :new_record?
 
    def set_default_role
     self.role ||= :user
-  end
-  
+   end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
